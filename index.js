8fcde432994847f6ab88ee1ad61e319571a3b805
Liar AKA hotdog sama <38080681+Liar-Sama@users.noreply.github.com>
2021-02-23 14:59:11 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,87 @@
-const cp = require('child_process')
-const path = require('path')
-const assert = require('assert')
-const { Vec3 } = require('vec3')
-const readFile = (fileName) => util.promisify(fs.readFile)(fileName, 'utf8')
+const mineflayer = require('mineflayer')
+const pvp = require('mineflayer-pvp').plugin
+const { pathfinder, Movements } = require('mineflayer-pathfinder')
+const { GoalInvert, GoalFollow } = require('mineflayer-pathfinder').goals
+const armorManager = require('mineflayer-armor-manager')
+const mineflayerViewer = require('prismarine-viewer').mineflayer
+const inventoryViewer = require('mineflayer-web-inventory')
 const events = require('events').EventEmitter.defaultMaxListeners = Infinity
 
 
-function startBot (botName, Password) {
-  const command = 'node ' + path.join(__dirname, 'start_bot') + ' ' + botName
-  cp.exec(command, (err, stdout, stderr) => {
-    if (err) {
-      console.log(`Error: ${err}`)
-      console.log(`Bot broken: ${botName}`)
-      console.log(`Restarting bot ${botName}...`)
-      setTimeout(() => startBot(botName, Password), 5000)
-      return
-    }
 
-    if (stdout) {
-      console.log(`Stdout: ${stdout}`)
+mineflayer.multiple = (bots, constructor) => {
+    const { Worker, isMainThread, workerData } = require('worker_threads')
+    if (isMainThread) {
+      const threads = []
+      for (const i in bots) {
+        threads.push(new Worker(__filename, { workerData: bots[i] }))
+      }
+    } else {
+      constructor(workerData)
     }
-
-    if (stderr) {
-      console.log(`Stderr: ${stderr}`)
-    }
-  })  
 }
-
-const botsToStart = [
-  { username: 'someone.lol123456@gmail.com' },
-  { username: 'idk.johnson.uno@gmail.com' },
-  { username: 'epicaa534@gmail.com' },
-  { username: 'hotdogsama22@gmail.com' },
-  { username: 'villzy.bizz@gmail.com' },
-  { username: 'doyoulovemeimasong@gmail.com' },
   
-]
-
-let i = 0
-function runNextBot () {
-  const botToStart = botsToStart[i]
-  i++
-  if (i <= botsToStart.length) {
-    setTimeout(() => {
-      startBot(botToStart.username)
-      runNextBot()
-    }, 6000) //I recommended you wait for dont stuck the server they can detect too many connections at same time
+  const bots = []
+  for (let i = 0; i < 1; i++) {
+    bots.push({ username: `bot${i}` })
   }
-};
+  
+  mineflayer.multiple(bots, ({ username }) => {
+    const bot = mineflayer.createBot({ username,  host: 'play.ultimismc.com', auth: 'tlauncher.org' })
 
-runNextBot()
+    
+
+    
+bot.once('spawn', () => {
+    bot.loadPlugin(pvp)
+    bot.loadPlugin(armorManager)
+    bot.loadPlugin(pathfinder)
+  // Once we've spawn, it is safe to access mcData because we know the version
+  const mcData = require('minecraft-data')(bot.version)
+  bot.chat('/login shaaat1223')
+  console.log('ok')
+  // We create different movement generators for different type of activity
+  const defaultMove = new Movements(bot, mcData, pvp)
+  defaultMove.allowFreeMotion = true
+  bot.pathfinder.searchRadius = 100
+
+  bot.on('path_update', (results) => {
+    console.log('[' + username + '] I can get there in ' + results.path.length + ' moves. Computation took ' + results.time.toFixed(2) + ' ms.')
+  })
+
+  bot.on('goal_reached', (goal) => {
+    console.log('[' + username + '] Here I am !')
+  })
+
+
+
+  
+  bot.on('chat', (username, message) => {
+    if (username === bot.players['__facts__']) return
+    const target = bot.players['__facts__'].entity
+
+    const filter = e => e.type === 'mob' && e.position.distanceTo(bot.entity.position) < 16 &&
+    e.mobType !== 'Armor Stand' // Mojang classifies armor stands as mobs for some reason?
+
+const entity = bot.nearestEntity(filter)
+
+    
+    if (message === 'bot.swarm') {
+      bot.pathfinder.setMovements(defaultMove)
+      bot.pathfinder.setGoal(new GoalFollow(target, 3), true)
+      bot.chat('command detected.')
+    } if (message === 'avoid') {
+      bot.pathfinder.setMovements (defaultMove)
+      bot.pathfinder.setGoal (new GoalInvert(new GoalFollow(target, 5)), true)
+    } else if (message === 'stop') {
+      bot.pathfinder.setGoal(null)
+    }
+  
+    
+    
+   
+
+ 
+  })
+  })
+  })
